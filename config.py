e50ac8cc86d3fa3ed4540680e598351aabdf5423
Alawi AlQurashi <126424764+AlawiAlQurashi@users.noreply.github.com>
2023-08-14 23:00:51 +0300

    update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://graph.org/file/4921b5d12ac8f7013ac73.jp
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/D_FBJ")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pp_g3")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SSLET")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FZKR3")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5190136458").split()))
 
